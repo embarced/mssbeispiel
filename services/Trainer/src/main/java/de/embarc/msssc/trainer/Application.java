8f5e08a99b89d873c111b192980c65fd0def2bec
Stefan Zoerner <stefan.zoerner@embarc.de>
2015-12-01 23:18:56 +0100

    Anmeldung an Eureka ergaenzt.

--- services/Trainer/src/main/java/de/embarc/msssc/trainer/Application.java
+++ services/Trainer/src/main/java/de/embarc/msssc/trainer/Application.java
@@ -3,9 +3,9 @@ package de.embarc.msssc.trainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

e85ae70e7d746d9d5588abc061eac7c79090a4ea
Stefan Zoerner <stefan.zoerner@embarc.de>
2015-12-01 23:16:56 +0100

    auskommentierten Code eliminiert

--- services/Trainer/src/main/java/de/embarc/msssc/trainer/TrainerService.java
+++ services/Trainer/src/main/java/de/embarc/msssc/trainer/TrainerService.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -19,20 +17,6 @@ public class TrainerService {
 
     @Autowired
     TrainerBackend backend;
-
-    /*
-    public TrainerService() {
-
-        List<TrainerInfo> alleTrainer = new ArrayList<>();
-
-        alleTrainer.add(new TrainerInfo("lr", "Lars", "Röwekamp", "open knowledge", "lars.roewekamp@openknowledge.de"));
-        alleTrainer.add(new TrainerInfo("ew", "Eberhard", "Wolff", "innoQ", "eberhard.wolff@gmail.com"));
-        alleTrainer.add(new TrainerInfo("st", "Stefan", "Toth", "embarc", "stefan.toth@embarc.de"));
-        alleTrainer.add(new TrainerInfo("sz", "Stefan", "Zörner", "embarc", "stefan.zoerner@embarc.de"));
-
-        trainerData = Collections.unmodifiableList(alleTrainer);
-    }
-    */
 
     @RequestMapping("/alleTrainer")
     public List<TrainerInfo> alleTrainer() {

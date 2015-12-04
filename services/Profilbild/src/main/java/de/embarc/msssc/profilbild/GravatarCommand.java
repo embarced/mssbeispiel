c1cdedbf13c192b55c51ac36eaf4234aa30d13ec
Stefan Zoerner <stefan.zoerner@embarc.de>
2015-12-04 20:21:12 +0100

    File-Laden umgebaut. Bytes jetzt nur 1x geladen.

--- services/Profilbild/src/main/java/de/embarc/msssc/profilbild/GravatarCommand.java
+++ services/Profilbild/src/main/java/de/embarc/msssc/profilbild/GravatarCommand.java
@@ -13,6 +13,15 @@ import java.io.InputStream;
 
 public class GravatarCommand extends HystrixCommand<Profilbild> {
 
+
+    private static byte[] STANDARD_RED_DATA;
+    private static byte[] STANDARD_BLACK_DATA;
+
+    static {
+        STANDARD_BLACK_DATA = bildLaden("images/standard_black_60x60.jpg");
+        STANDARD_RED_DATA = bildLaden("images/standard_red_60x60.jpg");
+    }
+
     private String email;
 
     public GravatarCommand(String email) {
@@ -31,7 +40,7 @@ public class GravatarCommand extends HystrixCommand<Profilbild> {
         Profilbild id = new Profilbild();
 
         if (data == null) {
-            data = bildLaden("images/standard_black_60x60.jpg");
+            data = STANDARD_BLACK_DATA;
             id.setInfo("kein Bild bei Gravatar gefunden");
         }
         id.setData(data);
@@ -42,23 +51,27 @@ public class GravatarCommand extends HystrixCommand<Profilbild> {
     @Override
     protected Profilbild getFallback() {
         Profilbild id = new Profilbild();
+
         id.setInfo("Fallback");
-        id.setData(bildLaden("images/standard_red_60x60.jpg"));
+        id.setData(STANDARD_RED_DATA);
+
         return id;
     }
 
-    private byte[] bildLaden(String dateiname) {
+    private static byte[] bildLaden(String dateiname) {
         try {
             InputStream stream = ClassLoader.getSystemResourceAsStream(dateiname);
             BufferedInputStream bis = new BufferedInputStream(stream);
+
             ByteArrayOutputStream target = new ByteArrayOutputStream(32000);
 
             int d = 0;
             while ((d = bis.read()) != -1) {
                 target.write(d);
             }
-            stream.close();
 
+            bis.close();
+            target.close();
             return target.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);

50a6e26ee2a060310f7745085aa2c9573ca74f3a
Stefan Zoerner <stefan.zoerner@embarc.de>
2015-12-07 11:52:11 +0100

    Edgeserver (Zuul) hinzugefuegt

---
+++ platform/edgeserver/src/main/java/de/embarc/msssc/edgeserver/Application.java
@@ -0,0 +1,13 @@
+package de.embarc.msssc.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

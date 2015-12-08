e17a78f0f25c6c50f48798c4f4b33cb6c2e59d41
Stefan Zoerner <stefan.zoerner@embarc.de>
2015-12-08 09:36:31 +0100

    Imports sortiert und Konfiguration ausgeduennt.

--- platform/serviceregistry/src/main/java/de/embarc/msssc/serviceregistry/Application.java
+++ platform/serviceregistry/src/main/java/de/embarc/msssc/serviceregistry/Application.java
@@ -1,6 +1,5 @@
 package de.embarc.msssc.serviceregistry;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
